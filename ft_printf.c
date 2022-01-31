04f24b4d76c1d86a982f65b341854c93fe8ba3e9
Jkutkut <jorgeregonzalez@gmail.com>
2022-01-31 13:48:32 +0100

    get_argc will be moved soon

--- ft_printf.c
+++ ft_printf.c
@@ -6,18 +6,20 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 08:19:08 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/31 10:15:13 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/01/31 13:31:13 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "libft.h"
 
-int	ft_get_argv(const char format)
+int	ft_get_argc(const char *format)
 {
-	size_t	i
+	size_t	i;
 	int		c;
 
+	if (format == NULL)
+		return (0);
 	i = 0;
 	c = 0;
 	while (format[i])
@@ -34,7 +36,7 @@ int	ft_printf(const char *format, ...)
 	va_list	valist;
 	int		argc;
 
-	argv = ft_get_argv(format);
+	argc = ft_get_argc(format);
 	ft_putstr_fd((char *) format, 1);
 	
 	printf("number of argv: %i", argc);
