b53bb9dc30ae47881134f7685e070aef51658fad
Jkutkut <jorgeregonzalez@gmail.com>
2022-01-31 09:35:28 +0100

    printing format

--- ft_printf.c
+++ ft_printf.c
@@ -6,14 +6,15 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 08:19:08 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/31 08:41:35 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/01/31 09:19:13 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include "libft.h"
 
 int	ft_printf(const char *format, ...)
 {
-	ft_putstr(format);
+	ft_putstr_fd((char *) format, 1);
 	return (0);
 }
