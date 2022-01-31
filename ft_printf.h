045aa8c3b9bcc0b1a5e7a3cf9522dae9af2c6cce
Jkutkut <jorgeregonzalez@gmail.com>
2022-01-31 13:46:00 +0100

    ft_hasany added

--- ft_printf.h
+++ ft_printf.h
@@ -6,19 +6,23 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 08:21:29 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/31 10:16:40 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/01/31 13:19:12 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h> // ! TODO DEBUG
+
 # define FLAGS_CONV "%"
 //# define FLAGS_CONV "%# +"
 # define FLAG_MIN_W ".-O"
 
 int		ft_printf(const char *, ...);
-int	ft_get_argv(const char format);
-
+int		ft_get_argc(const char *format);
+int		ft_hasany(const char *str, const char c);
 #endif
 
