2a0850ae837ebde839311620749e8075c259e883
Jkutkut <jorgeregonzalez@gmail.com>
2022-01-31 11:05:28 +0100

    ready to start working on get_argc

--- ft_printf.c
+++ ft_printf.c
@@ -6,15 +6,38 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 08:19:08 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/31 09:19:13 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/01/31 10:15:13 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "libft.h"
 
+int	ft_get_argv(const char format)
+{
+	size_t	i
+	int		c;
+
+	i = 0;
+	c = 0;
+	while (format[i])
+	{
+		if (ft_hasany(FLAGS_CONV, format[i]))
+			c++;
+		i++;
+	}
+	return (c);
+}
+
 int	ft_printf(const char *format, ...)
 {
+	va_list	valist;
+	int		argc;
+
+	argv = ft_get_argv(format);
 	ft_putstr_fd((char *) format, 1);
+	
+	printf("number of argv: %i", argc);
+	
 	return (0);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,14 +6,19 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 08:21:29 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/31 08:22:34 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/01/31 10:16:40 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
+# define FLAGS_CONV "%"
+//# define FLAGS_CONV "%# +"
+# define FLAG_MIN_W ".-O"
+
 int		ft_printf(const char *, ...);
+int	ft_get_argv(const char format);
 
 #endif
 
