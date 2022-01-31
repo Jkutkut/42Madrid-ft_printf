06b94eac17c60bd629daa3f685a56cb21198e202
Jkutkut <jorgeregonzalez@gmail.com>
2022-01-31 13:40:08 +0100

    working fine

--- tools/ft_hasany.c
+++ tools/ft_hasany.c
@@ -6,13 +6,13 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 13:03:52 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/31 13:05:26 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/01/31 13:15:09 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-int	ft_hasany(const char *str, char c)
+int	ft_hasany(const char *str, const char c)
 {
 	size_t	i;
 
