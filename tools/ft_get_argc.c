9565f3d839fc610aedd04e2fda0f87d29f87755f
Jkutkut <jorgeregonzalez@gmail.com>
2022-02-01 09:30:21 +0100

    Now get_argc only counts valid elements

--- tools/ft_get_argc.c
+++ tools/ft_get_argc.c
@@ -6,11 +6,13 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 08:19:08 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/02/01 09:06:27 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/02/01 09:25:14 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+# define VALID_CONV "cspdiuxX%"
 
 int	ft_get_argc(const char *format)
 {
@@ -23,11 +25,13 @@ int	ft_get_argc(const char *format)
 	c = 0;
 	while (format[i])
 	{
-		if (ft_hasany(FLAGS_CONV, format[i++]))
+		if (ft_hasany(FLAGS_CONV, format[i]) &&
+			format[i + 1] && ft_hasany(VALID_CONV, format[i + 1]))
 		{
 			c++;
 			i++;
 		}
+		i++;
 	}
 	return (c);
 }
