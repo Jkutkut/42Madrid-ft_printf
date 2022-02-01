0a1ffe5aebd2575d1dcf95d87d4764f41ac1606e
Jkutkut <jorgeregonzalez@gmail.com>
2022-02-01 07:55:09 +0100

    get_argc now on independent file

---
+++ tools/ft_get_argc.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_get_argc.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jre-gonz <jre-gonz@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/31 08:19:08 by jre-gonz          #+#    #+#             */
+/*   Updated: 2022/01/31 13:53:12 by jre-gonz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_get_argc(const char *format)
+{
+	size_t	i;
+	int		c;
+
+	if (format == NULL)
+		return (0);
+	i = 0;
+	c = 0;
+	while (format[i])
+	{
+		if (ft_hasany(FLAGS_CONV, format[i]))
+			c++;
+		i++;
+	}
+	return (c);
+}
+
