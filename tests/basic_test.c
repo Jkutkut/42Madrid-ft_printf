2b4961370deeb20235f2f4cc78352240aa7e4541
Jkutkut <jorgeregonzalez@gmail.com>
2022-02-01 10:57:32 +0100

    tests with multiple arguments added

--- tests/basic_test.c
+++ tests/basic_test.c
@@ -3,18 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   basic_test.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jre-gonz <jre-gonz@student.42madrid.com>   +#+  +:+       +#+        */
+/*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 09:28:19 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/31 09:29:15 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/02/01 10:46:09 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "ft_printf.h"
 
+#include "colors.h"
+
 int	main(void)
 {
+	printf("%sBasic test:%s\n", TITLE, NC);
+	printf("%s---------------------------------%s\n", YELLOW, NC);
 	ft_printf("hola caracola\n");
+	printf("%s---------------------------------%s\n", YELLOW, NC);
+	ft_printf("%s %s %s\n", "hola", "caracola", "que tal?");
+	printf("%s---------------------------------%s\n", YELLOW, NC);
+	ft_printf("hola caracola\n", "hola", "caracola", 123);
+	printf("%s---------------------------------%s\n", YELLOW, NC);
 	return (0);
 }
