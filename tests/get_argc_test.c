626d43e4321fd282a99d23cf6bde14a83680e970
Jkutkut <jorgeregonzalez@gmail.com>
2022-02-01 09:29:53 +0100

    test updated with correct spacing and tests

--- tests/get_argc_test.c
+++ tests/get_argc_test.c
@@ -6,16 +6,17 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 11:05:46 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/02/01 09:18:23 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/02/01 09:29:07 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "libft.h"
 
-#define TEST_L 31
+#define TEST_1_L 31
+#define TEST_2_L 2
 
-void	test(const char *str, int spected)
+void	test(const char *str, int spected, const int TEST_L)
 {
 	int		result;
 	size_t	l;
@@ -40,33 +41,46 @@ void	test(const char *str, int spected)
 	}
 }
 
+void	test1(const char *str, int spected)
+{
+	test(str, spected, TEST_1_L);
+}
+
+void	test2(const char *str, int spected)
+{
+	test(str, spected, TEST_2_L);
+}
+
 int	main(void)
 {
 	printf("\n\033[1;33mTest ft_get_argc\n\033[0m");
-	test("hola que tal", 0);
-	test("", 0);
-	test(NULL, 0);
-	test("hola%iquetal", 1);
-	test("%c start", 1);
-	test("end %s", 1);
-	test("%s start and end %i", 2);
-	test("percent %% percent", 1);
-	test("%s start, percent %% and end %i", 3);
+	test1("hola que tal", 0);
+	test1("", 0);
+	test1(NULL, 0);
+	test1("hola%iquetal", 1);
+	test1("%c start", 1);
+	test1("end %s", 1);
+	test1("%s start and end %i", 2);
+	test1("percent %% percent", 1);
+	test1("%s start, percent %% and end %i", 3);
 
 	printf("\n\033[1;33mTest ft_get_argc with conversions\n\033[0m");
-	test("%c", 1);
-	test("%s", 1);
-	test("%p", 1);
-	test("%d", 1);
-	test("%i", 1);
-	test("%u", 1);
-	test("%x", 1);
-	test("%X", 1);
-	test("%%", 1);
+	test2("%c", 1);
+	test2("%s", 1);
+	test2("%p", 1);
+	test2("%d", 1);
+	test2("%i", 1);
+	test2("%u", 1);
+	test2("%x", 1);
+	test2("%X", 1);
+	test2("%%", 1);
 
-	test("%a", 0);
-	test("%b", 0);
-	test("%d", 0);
-	test("%e", 0);
+	test2("%a", 0);
+	test2("%b", 0);
+	test2("%e", 0);
+	test2("%f", 0);
+
+	// Print a message in green
+	printf("\nAll tests \033[0;32mOK\n\033[0m");
 }
 
