e8713483f988772905b157ea76e7af5eeda51cdc
Jkutkut <jorgeregonzalez@gmail.com>
2022-02-01 09:18:59 +0100

    test updated with more tests

--- tests/get_argc_test.c
+++ tests/get_argc_test.c
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   get_argc_test.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jre-gonz <jre-gonz@student.42madrid.com>   +#+  +:+       +#+        */
+/*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 11:05:46 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/31 13:39:33 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/02/01 09:18:23 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "libft.h"
 
-#define TEST_L 30
+#define TEST_L 31
 
 void	test(const char *str, int spected)
 {
@@ -24,6 +24,7 @@ void	test(const char *str, int spected)
 		l = TEST_L - ft_strlen(str);
 	else
 		l = TEST_L - ft_strlen("(NULL)");
+
 	printf("Test: ");
 	while (l--)
 		printf(" ");
@@ -41,8 +42,31 @@ void	test(const char *str, int spected)
 
 int	main(void)
 {
+	printf("\n\033[1;33mTest ft_get_argc\n\033[0m");
 	test("hola que tal", 0);
 	test("", 0);
 	test(NULL, 0);
+	test("hola%iquetal", 1);
+	test("%c start", 1);
+	test("end %s", 1);
+	test("%s start and end %i", 2);
+	test("percent %% percent", 1);
+	test("%s start, percent %% and end %i", 3);
+
+	printf("\n\033[1;33mTest ft_get_argc with conversions\n\033[0m");
+	test("%c", 1);
+	test("%s", 1);
+	test("%p", 1);
+	test("%d", 1);
+	test("%i", 1);
+	test("%u", 1);
+	test("%x", 1);
+	test("%X", 1);
+	test("%%", 1);
+
+	test("%a", 0);
+	test("%b", 0);
+	test("%d", 0);
+	test("%e", 0);
 }
 
