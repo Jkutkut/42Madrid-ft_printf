7658e31cee0c79365982da3179dc5830c0ed8c49
Jkutkut <jorgeregonzalez@gmail.com>
2022-02-01 13:06:33 +0100

    working on a standard way to develop printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,32 +6,62 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 08:19:08 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/02/01 10:46:52 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/02/01 12:29:26 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "libft.h"
 
+int	ft_print_until_format(const char **format)
+{
+	size_t i;
+
+	if (!*format)
+		return (-1);
+	i = 0;
+	while (*format[i] && *format[i] != '%')
+		i++;
+	ft_putnstr(*format, i);
+	return (i);
+
+}
+
+int	ft_format_printf(const char *format, va_list lst)
+{
+	int i;
+
+	i = 0;
+	while (*format)
+	{
+		if ((i = ft_print_until_format(&format)) < 0 || !*format)
+			return (i);
+		break;
+	}
+	return (i);
+}
+
 int	ft_printf(const char *format, ...)
 {
-	va_list	valist;
+	va_list	argv;
 	int		argc;
-	char	*tmp;
+	int		printed;
+	// char	*tmp;
 
 	argc = ft_get_argc(format);
-	printf("number of argv: %i\n", argc);
+	printf("number of argvs: %i\n", argc);
 	printf("Arguments:\n");
 
-	va_start(valist, format);
+	va_start(argv, format);
 
 	// while (argc--)
-	while ((tmp = va_arg(valist, char *)) != NULL)
-	{
-		printf("%s\n", tmp);
-	}
-	va_end(valist);
+	// while ((tmp = va_arg(valist, char *)) != NULL)
+	// {
+	// 	printf("%s\n", tmp);
+	// }
+	printed = ft_format_printf(format, argv);
+	va_end(argv);
 	// ft_putstr_fd((char *) format, 1);
 	
-	return (0);
+	return (printed);
 }
