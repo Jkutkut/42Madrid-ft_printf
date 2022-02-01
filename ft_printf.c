8b3ca2db68f6e46241f5af7783053679d8b816e9
Jkutkut <jorgeregonzalez@gmail.com>
2022-02-01 13:35:26 +0100

    doing some testing to see if this aproach to implement printf could work.

--- ft_printf.c
+++ ft_printf.c
@@ -6,36 +6,41 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 08:19:08 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/02/01 12:29:26 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/02/01 13:33:19 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "libft.h"
 
-int	ft_print_until_format(const char **format)
+int	ft_print_until_format(char **format)
 {
-	size_t i;
+	size_t	i;
+	char	*str;
 
-	if (!*format)
+	if (format == NULL || *format == NULL)
 		return (-1);
+	str = *format;
 	i = 0;
-	while (*format[i] && *format[i] != '%')
+	while (str[i] && str[i] != '%')
 		i++;
-	ft_putnstr(*format, i);
+	ft_putnstr(str, i);
+	str += i;
 	return (i);
 
 }
 
-int	ft_format_printf(const char *format, va_list lst)
+int	ft_format_printf(char *format, va_list lst)
 {
 	int i;
 
 	i = 0;
 	while (*format)
 	{
+		printf("IN\n");
 		if ((i = ft_print_until_format(&format)) < 0 || !*format)
 			return (i);
+		printf("%s", va_arg(lst, char *));
 		break;
 	}
 	return (i);
@@ -46,9 +51,12 @@ int	ft_printf(const char *format, ...)
 	va_list	argv;
 	int		argc;
 	int		printed;
-	// char	*tmp;
+	char	*format_cpy;
 
-	argc = ft_get_argc(format);
+	if (format == NULL)
+		return (-1);
+	format_cpy = ft_strdup(format);
+	argc = ft_get_argc(format_cpy);
 	printf("number of argvs: %i\n", argc);
 	printf("Arguments:\n");
 
@@ -59,8 +67,9 @@ int	ft_printf(const char *format, ...)
 	// {
 	// 	printf("%s\n", tmp);
 	// }
-	printed = ft_format_printf(format, argv);
+	printed = ft_format_printf(format_cpy, argv);
 	va_end(argv);
+	free(format_cpy);
 	// ft_putstr_fd((char *) format, 1);
 	
 	return (printed);
