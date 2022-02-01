0ac1990c960013d52c1107bb25b094896634bf78
Jkutkut <jorgeregonzalez@gmail.com>
2022-02-01 07:55:40 +0100

    get_argc now on indenpendent file

--- ft_printf.c
+++ ft_printf.c
@@ -6,30 +6,12 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 08:19:08 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/31 13:31:13 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/01/31 13:52:13 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "libft.h"
-
-int	ft_get_argc(const char *format)
-{
-	size_t	i;
-	int		c;
-
-	if (format == NULL)
-		return (0);
-	i = 0;
-	c = 0;
-	while (format[i])
-	{
-		if (ft_hasany(FLAGS_CONV, format[i]))
-			c++;
-		i++;
-	}
-	return (c);
-}
 
 int	ft_printf(const char *format, ...)
 {
