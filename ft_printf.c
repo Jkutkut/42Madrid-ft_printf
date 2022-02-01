df12c8271fa66f0ea2b9c00df19db34ed759d6b3
Jkutkut <jorgeregonzalez@gmail.com>
2022-02-01 10:57:55 +0100

    now able to read multiple arguments

--- ft_printf.c
+++ ft_printf.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jre-gonz <jre-gonz@student.42madrid.com>   +#+  +:+       +#+        */
+/*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 08:19:08 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/31 13:52:13 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/02/01 10:46:52 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,21 @@ int	ft_printf(const char *format, ...)
 {
 	va_list	valist;
 	int		argc;
+	char	*tmp;
 
 	argc = ft_get_argc(format);
-	ft_putstr_fd((char *) format, 1);
-	
-	printf("number of argv: %i", argc);
+	printf("number of argv: %i\n", argc);
+	printf("Arguments:\n");
+
+	va_start(valist, format);
+
+	// while (argc--)
+	while ((tmp = va_arg(valist, char *)) != NULL)
+	{
+		printf("%s\n", tmp);
+	}
+	va_end(valist);
+	// ft_putstr_fd((char *) format, 1);
 	
 	return (0);
 }
