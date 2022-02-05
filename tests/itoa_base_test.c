099b9e43395413ff8cade01852f22bbb8453e1d5
Jkutkut <jorgeregonzalez@gmail.com>
2022-02-06 00:17:21 +0100

    itoa_base implemented

--- tests/itoa_base_test.c
+++ tests/itoa_base_test.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 22:34:52 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/02/05 22:56:12 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/02/06 00:16:44 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	test(long int i, char *base)
 	char *str;
 
 	str = ft_itoa_base(i, base);
-	printf("%li in base \"%s\" : \"%s\"\n", i, base, str);
+	printf("Base \"%s\" %li: \"%s\"\n", base, i, str);
 	free(str);
 }
 
@@ -25,8 +25,31 @@ int	main(void)
 {
 	test(0, "0123456789");
 	test(1, "0123456789");
-	test(2, "0123456789");
 	test(9, "0123456789");
 	test(10, "0123456789");
+	test(-1, "0123456789");
+	test(-10, "0123456789");
+
+	test(0, "0123456789abcdef");
+	test(1, "0123456789abcdef");
+	test(9, "0123456789abcdef");
+	test(10, "0123456789abcdef");
+	test(15, "0123456789abcdef");
+	test(16, "0123456789abcdef");
+	test(-1, "0123456789abcdef");
+	test(-10, "0123456789abcdef");
+	test(-15, "0123456789abcdef");
+	test(-16, "0123456789abcdef");
+
+	test(0, "01");
+	test(1, "01");
+	test(9, "01");
+	test(10, "01");
+	test(15, "01");
+	test(16, "01");
+	test(-1, "01");
+	test(-10, "01");
+	test(-15, "01");
+	test(-16, "01");
 	return (0);
 }
--- tools/ft_itoa_base.c
+++ tools/ft_itoa_base.c
@@ -6,10 +6,11 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 22:38:02 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/02/05 23:14:05 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/02/06 00:17:05 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 #include <stdlib.h>
 
 size_t	ft_strlen(const char *s);
@@ -17,7 +18,7 @@ size_t	ft_nbrlen(size_t n, size_t base_len);
 
 char	*ft_itoa_base(ssize_t nbr, char *base)
 {
-	size_t	b_len;
+	ssize_t	b_len;
 	size_t	order;
 	char	*str;
 	char	sign;
@@ -30,12 +31,15 @@ char	*ft_itoa_base(ssize_t nbr, char *base)
 	str[order--] = '\0';
 	sign = 1;
 	if (nbr < 0)
+	{
 		sign = -1;
+		str[0] = '-';
+	}
 	if (nbr == 0)
 		str[order] = '0';
 	while (nbr != 0)
 	{
-		str[order] = base[(nbr % b_len) * sign];
+		str[order--] = base[((ssize_t) nbr % b_len) * sign];
 		nbr /= b_len;
 	}
 	return (str);
