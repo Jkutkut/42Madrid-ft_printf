eb845942986d1bc1e676f39038ec308fc07ac24c
Jkutkut <jorgeregonzalez@gmail.com>
2022-02-05 23:58:01 +0100

    fixed

--- tools/ft_nbrlen.c
+++ tools/ft_nbrlen.c
@@ -6,17 +6,17 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 22:41:50 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/02/05 23:30:13 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/02/05 23:54:33 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-size_t	ft_nbrlen(ssize_t n, size_t base_len)
+size_t	ft_nbrlen(ssize_t n, ssize_t base_len)
 {
 	size_t	order;
 
-	if (!base_len)
+	if (base_len <= 0)
 		return (0);
 	if (!n)
 		return (1);
