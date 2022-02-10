3c6b97f5149807b8ecfbd19a67496e76d1303305
Jkutkut <jorgeregonzalez@gmail.com>
2022-02-10 23:13:05 +0100

    export with minimal infor

--- ft_printf.h
+++ ft_printf.h
@@ -5,44 +5,19 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/31 08:21:29 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/02/08 15:43:33 by jre-gonz         ###   ########.fr       */
+/*   Created: 2022/02/10 23:10:35 by jre-gonz          #+#    #+#             */
+/*   Updated: 2022/02/10 23:11:58 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-#include <stdarg.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdlib.h>
+#include <stdarg.h> // va_list
+#include <stdlib.h> // malloc free
+#include <unistd.h> // write
 #include <stdio.h> // ! TODO DEBUG
 
-#include "libft.h"
-
-# define FLAGS_CONV "%"
-//# define FLAGS_CONV "%# +"
-# define FLAG_MIN_W ".-O"
-
 int		ft_printf(const char *, ...);
-int		ft_get_argc(const char *format);
-
-// Tools
-// int	ft_get_argc(const char *format);
-int		ft_hasany(const char *str, const char c);
-char	*ft_itoa_base(ssize_t nbr, char *base);
-size_t	ft_nbrlen(ssize_t n, size_t base_len);
-void	ft_putnstr(const char *str, int n);
-
-typedef struct	s_printf_tk
-{
-	char	type;
-	char	left_padding;
-	char	fill_type;
-	char	
-	char	precision;
-}	t_printf_tk;
 
 #endif
-
