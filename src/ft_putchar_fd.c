43d49a93682ac44e729f62f4265d4ebfefa75ae6
Jkutkut <jorgeregonzalez@gmail.com>
2022-02-13 21:20:23 +0100

    now returning size_t to follow the logic of all code

--- src/ft_putchar_fd.c
+++ src/ft_putchar_fd.c
@@ -6,13 +6,14 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/25 08:54:24 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/01/26 10:44:38 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/02/13 21:18:13 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putchar_fd(char c, int fd)
+size_t	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
+	return (1);
 }
