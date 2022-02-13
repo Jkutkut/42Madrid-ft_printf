01dc15515e271c13ffaaebb91474e4a7929bc954
Jkutkut <jorgeregonzalez@gmail.com>
2022-02-13 22:44:18 +0100

    now using a function to uppercase a sentence directly

--- src/ft_strtoupper.c
+++ src/ft_strtoupper.c
@@ -1,18 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_toupper.c                                       :+:      :+:    :+:   */
+/*   ft_strtoupper.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 16:29:11 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/02/13 15:29:30 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/02/13 22:42:57 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_toupper(int c)
+static int	ft_toupper(int c)
 {
 	if (c >= 'a' && c <= 'z')
 		return (c - 32);
 	return (c);
+}
+
+int	ft_strtoupper(char *str)
+{
+	while (*str)
+	{
+		*str = ft_toupper(*str);
+		str++;
+	}
 }
