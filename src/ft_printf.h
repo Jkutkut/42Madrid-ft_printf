ea220d423bdf4ca0514845e3bdbc6bb7b417950e
Jkutkut <jorgeregonzalez@gmail.com>
2022-02-13 21:19:55 +0100

    functions updated

--- src/ft_printf.h
+++ src/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 23:10:35 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/02/13 16:12:22 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/02/13 21:18:32 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@
 char	*ft_itoa_base(ssize_t nbr, char *base);
 size_t	ft_nbrlen(ssize_t n, ssize_t base_len);
 int		ft_printf(const char *, ...);
-void	ft_putchar_fd(char s, int fd);
-void	ft_putnbr_fd(int n, int fd);
+size_t	ft_putchar_fd(char s, int fd);
+// void	ft_putnbr_fd(int n, int fd);
 size_t	ft_putstr_fd(char *s, int fd);
 char	*ft_strdup(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
