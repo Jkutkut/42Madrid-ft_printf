cbc379724562a6643ec5aa30dc3ebb0d961b9e16
Jkutkut <jorgeregonzalez@gmail.com>
2022-02-13 15:26:09 +0100

    moving code to src

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,24 +6,149 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 22:07:03 by jkutkut           #+#    #+#             */
-/*   Updated: 2022/02/13 14:55:55 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/02/13 15:19:40 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+#define DECIMAL "0123456789"
+#define HEXADECIMAL "0123456789abcdef"
+
+int	ft_print_argv(char **format, va_list lst)
+{
+	int	i;
+	char	*str;
+
+	i = -1;
+	if (ft_strncmp(*format, "%c", 2) == 0)
+	{
+		*format += 2;
+		i = 1;
+		ft_putchar_fd(va_arg(lst, int), 1);
+	}
+	else if (ft_strncmp(*format, "%s", 2) == 0)
+	{
+		*format += 2;
+		str = va_arg(lst, char *);
+		i = ft_strlen(str);
+		ft_putnstr(str, i);
+	}
+	else if (ft_strncmp(*format, "%p", 2) == 0)
+	{
+		*format += 2;
+		str = ft_itoa_base(va_arg(lst, unsigned long), HEXADECIMAL);
+		i = ft_strlen(str) + 2;
+		ft_putnstr("0x", 2);
+		ft_putnstr(str, i);
+		free(str);
+	}
+	else if (ft_strncmp(*format, "%d", 2) == 0)
+	{
+		*format += 2;
+		str = ft_itoa_base(va_arg(lst, long), DECIMAL);
+		ft_putstr_fd(str, 1);
+		i = ft_strlen(str);
+		free(str);
+	}
+	else if (ft_strncmp(*format, "%i", 2) == 0)
+	{
+		*format += 2;
+		str = ft_itoa_base(va_arg(lst, int), DECIMAL);
+		ft_putstr_fd(str, 1);
+		i = ft_strlen(str);
+		free(str);
+	}
+	else if (ft_strncmp(*format, "%u", 2) == 0)
+	{
+		*format += 2;
+		str = ft_itoa_base(va_arg(lst, unsigned long), DECIMAL);
+		ft_putstr_fd(str, 1);
+		i = ft_strlen(str);
+		free(str);
+	}
+	else if (ft_strncmp(*format, "%%", 2) == 0)
+	{
+		*format += 2;
+		i += 1;
+		ft_putchar_fd('%', 1);
+	}
+	else // %x %X
+	{
+		*format += 2;
+		str = ft_itoa_base(va_arg(lst, unsigned long), HEXADECIMAL);
+		i = ft_strlen(str) + 2;
+		write(1, "0x", 2);
+		if (ft_strncmp(*(format - 1), "X", 2) == 0)
+		{
+			size_t	j;
+
+			j = 0;
+			while (str[j])
+			{
+				str[j] = ft_toupper(str[j]);
+				j++;
+			}
+		}
+	}
+	
+	return (i);
+}
+
+int	ft_print_until_format(char **format)
+{
+	int	i;
+	char	*str;
+
+	if (format == NULL || *format == NULL)
+		return (-1);
+	str = *format;
+	i = 0;
+	while (str[i] && str[i] != '%')
+		i++;
+	write(1, str, i);
+	*format += i;
+	return (i);
+
+}
+
+int	ft_format_printf(char *format, va_list lst)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (*format)
+	{
+		j = ft_print_until_format(&format);
+		if (j < 0)
+			return (i);
+		i += j;
+		if (*format == '\0')
+			return (i);
+		j = ft_print_argv(&format, lst);
+		if (j < 0)
+		{
+			ft_putstr_fd("not implemented", 1);
+			break;
+		}
+		i += j;
+	}
+	return (i);
+}
+
 int	ft_printf(const char *format, ...)
 {
 	va_list	argv;
-	int		argc;
+	// int		argc;
 	int		printed;
 	char	*format_cpy;
 
 	if (format == NULL)
 		return (-1);
 	format_cpy = ft_strdup(format);
-	argc = ft_get_argc(format_cpy);
-	printf("- format: \"%s\"\n- Number of argvs: %i\n\n", format, argc);
+	// argc = ft_get_argc(format_cpy);
+	// printf("- format: \"%s\"\n- Number of argvs: %i\n\n", format, argc);
 	va_start(argv, format);
 	printed = ft_format_printf(format_cpy, argv);
 	va_end(argv);
--- src/ft_printf.h
+++ src/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 23:10:35 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/02/10 23:11:58 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/02/13 15:24:51 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,16 @@
 #include <unistd.h> // write
 #include <stdio.h> // ! TODO DEBUG
 
+char	*ft_itoa_base(ssize_t nbr, char *base);
+size_t	ft_nbrlen(ssize_t n, ssize_t base_len);
 int		ft_printf(const char *, ...);
+void	ft_putchar_fd(char s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putstr_fd(char *s, int fd);
+char	*ft_strdup(const char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlen(const char *s);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
+
 
 #endif
