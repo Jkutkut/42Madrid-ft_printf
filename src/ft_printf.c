6917bf77c2feaeffb160b1a8dba45b0bdc372a0d
Jkutkut <jre-gonz@student.42madrid.com>
2022-02-14 15:40:37 +0100

    norminette OK

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,16 +6,35 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 22:07:03 by jkutkut           #+#    #+#             */
-/*   Updated: 2022/02/13 23:05:24 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/02/14 15:40:14 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+int	ft_print_number(char type, va_list lst)
+{
+	char	*str;
+	int		i;
+
+	if (type == 'u')
+		str = ft_itoa_base(va_arg(lst, unsigned int), DECIMAL);
+	else if (ft_hasany("Xx", type))
+	{
+		str = ft_itoa_base(va_arg(lst, unsigned int), HEXADECIMAL);
+		if (type == 'X')
+			ft_strtoupper(str);
+	}
+	else
+		str = ft_itoa_base(va_arg(lst, int), DECIMAL);
+	i = ft_putstr_fd(str, 1);
+	free(str);
+	return (i);
+}
+
 int	ft_print_argv(char **format, va_list lst)
 {
 	int		i;
-	char	*str;
 
 	if (*++(*format) == 'c')
 		i = ft_putchar_fd(va_arg(lst, int), 1);
@@ -23,25 +42,10 @@ int	ft_print_argv(char **format, va_list lst)
 		i = ft_putstr_fd(va_arg(lst, char *), 1);
 	else if (**format == 'p')
 		i = ft_put_pointer_fd(va_arg(lst, unsigned long), 1);
-	else if (ft_hasany("idu", **format))
-	{
-		if (**format == 'u')
-			str = ft_itoa_base(va_arg(lst, unsigned int), DECIMAL);
-		else
-			str = ft_itoa_base(va_arg(lst, int), DECIMAL);
-		i = ft_putstr_fd(str, 1);
-		free(str);
-	}
-	else if (**format == '%')
-		i = ft_putchar_fd('%', 1);
+	else if (ft_hasany("iduxX", **format))
+		i = ft_print_number(**format, lst);
 	else
-	{
-		str = ft_itoa_base(va_arg(lst, unsigned int), HEXADECIMAL);
-		if (**format == 'X')
-			ft_strtoupper(str);
-		i = ft_putstr_fd(str, 1);
-		free(str);
-	}
+		i = ft_putchar_fd('%', 1);
 	(*format)++;
 	return (i);
 }
